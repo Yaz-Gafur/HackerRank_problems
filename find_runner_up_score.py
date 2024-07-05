0634f3e8530a212f0ce5cb7a476110e9db7f1fb1
Yaz-Gafur <140863760+Yaz-Gafur@users.noreply.github.com>
2024-07-05 21:26:12 +0500

    Create find_runner_up_score.py

---
+++ find_runner_up_score.py
@@ -0,0 +1,18 @@
+def find_runner_up_score(scores):
+    max_score = float('-inf')
+    runner_up_score = float('-inf')
+
+    for score in scores:
+        if score > max_score:
+            runner_up_score = max_score
+            max_score = score 
+        elif score > runner_up_score and score != runner_up_score:
+            runner_up_score = score
+
+        return runner_up_score
+if __name__ == '__main__':
+    n = int(input())
+    scores = list(map(int, input().split()))
+    
+    runner_up = find_runner_up_score(scores)
+    print(runner_up)
