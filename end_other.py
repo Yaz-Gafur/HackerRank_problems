3e53042047ffb80bcf187fcf5bd845abb9fa2a00
Yaz-Gafur <140863760+Yaz-Gafur@users.noreply.github.com>
2024-08-11 17:48:12 +0500

    Update end_other.py
    simplified

--- end_other.py
+++ end_other.py
@@ -1,6 +1,7 @@
 def end_other(a, b):
   a = a.lower()
   b = b.lower()
-  if a.endswith(b) or b.endswith(a):
+  if a.endswith(b) or b.endswith(a):     # return (a.endswith(b) or b.endswith(a)) ----> same result
     return True
   return False
+
